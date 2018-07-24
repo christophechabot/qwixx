d110f7c7192513b1ddf24a82417ac382b3df8f7b
Christophe Chabot <christophe.chabot.wd@gmail.com>
2018-07-24 21:51:56 +0100

    Added docs folder to build the github page

---
+++ docs/precache-manifest.558060082a5ccce866fd9394c5c549fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://github.com/christophechabot/qwixx/robots.txt"
+  },
+  {
+    "revision": "054a6339167fa45cbd24",
+    "url": "https://github.com/christophechabot/qwixx/js/chunk-vendors.054a6339.js"
+  },
+  {
+    "revision": "a55250e481bcfa279f4a",
+    "url": "https://github.com/christophechabot/qwixx/js/app.a55250e4.js"
+  },
+  {
+    "revision": "9e44d4d0eaf2c53295f23035cca6613c",
+    "url": "https://github.com/christophechabot/qwixx/index.html"
+  },
+  {
+    "revision": "a55250e481bcfa279f4a",
+    "url": "https://github.com/christophechabot/qwixx/css/app.af12320c.css"
+  }
+];
